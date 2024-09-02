f94a306a7119250ab2f70306148e26e92b7b315d
mohamed <khaldimohamed82@gmail.com>
2024-09-02 20:24:57 +0100

    Expanding my Skillset, Embracing Continuous Learning

---
+++ front-end-Meta/work/FP.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// Task 1: Build a function-based console log message generator
+
+function consoleStyler(color, background, fontSize, txt) {
+    var message = "%c" + txt;
+    var style = `color: ${color};`;
+    style += `background: ${background};`;
+    style += `font-size: ${fontSize};`;
+    
+    console.log(message, style);
+}
+consoleStyler("red", "white", "bold", "heloo");
